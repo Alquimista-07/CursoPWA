234c5d0f3f16bef8b6f4c940648cbb2a6de66c5c
Alquimista-07 <jdca.07@gmail.com>
2022-02-21 15:47:39 -0500

    Ejercicios explicativos de formas validas para realizar peticiones desde el evento Fetch

--- 02-service-worker/Sw.js
+++ 02-service-worker/Sw.js
@@ -9,11 +9,26 @@ self.addEventListener('fetch', evento => {
     //console.log(evento);
 
     //Acá por ejemplo lo que vamos a hacer e bloquear a través del SW la petición al archivo style.css
+    /*
     if(evento.request.url.includes('style.css')){
+        //La siguiente instrucción de codigo va a causar que la petición responda null
+        //y por lo tanto no va a ser llevada a cabo
         evento.respondWith(null);
     }
     else{
         evento.respondWith(fetch(evento.request));
+    }
+    */
+
+    //Acá por ejemplo vamos a hacer algo con las imágenes
+    if(evento.request.url.includes('.jpg')){
+        console.log(evento.request.url);
+        // Formas de hacer las peticiones
+        //let fotoReq = fetch('img/main.jpg');
+        //let fotoReq = fetch(evento.request.url);
+        let fotoReq = fetch(evento.request);
+
+        evento.respondWith(fotoReq);
     }
 
 });
