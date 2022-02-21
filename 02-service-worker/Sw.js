84c0a3964e12c608933b6aa50e552631f40bc601
Alquimista-07 <jdca.07@gmail.com>
2022-02-21 15:25:03 -0500

    Agregado jshintrc con configuración del tshint, adicionalmente se modifica el SW para prepararlo y llamar el primer evento

--- 02-service-worker/Sw.js
+++ 02-service-worker/Sw.js
@@ -1,1 +1,11 @@
-console.log('SW: Hola Mundo!!!...');
+//console.log('SW: Hola Mundo!!!...');
+
+// No es normal escribir codigo JavaScript dentro de este archivo, es decir, que no se pueda hacer
+// pero normalmente el Service Worker (SW) va a contener acciones a sucesos que sucedan en la página o en nuestra aplicación
+
+//Usamos la palabra self para hacer referencia al SW
+self.addEventListener('fetch', evento => {
+
+    console.log(evento);
+
+});
