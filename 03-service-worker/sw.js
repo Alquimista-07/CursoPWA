385547d560419ff194c01810e7955051cf514e5d
Alquimista-07 <jdca.07@gmail.com>
2022-02-22 10:41:53 -0500

    Agregada explicacion del evento waitUntil y notas importantes sobre este evento

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -20,6 +20,26 @@ self.addEventListener('install', evento => {
     //NOTA: No es recomendable dejarlo porque puede ser que el usuario este trabajando con algo del SW, por ejemplo recibir una notificación push,
     //o algo importante que puede estar sucediendo que le interesa al usuario pero como saltamos lo del anterior SW pueda ser que no ejecute el codigo y
     //perder la información importante que el cliente este esperando, lo mejor es que el cliente cierr la app web para que el nuevo SW entre en funcionamiento
+
+    const instalacion = new Promise((resolve, reject) => {
+
+        //Simulación de que nuestra instalación se tarda un segundo
+        setTimeout(() => {
+            console.log('SW: Instalaciones terminadas');
+            //Usamos la linea dar skip cuando se detecta un nuevo SW y la cual se explico anteriormente
+            self.skipWaiting();
+            //Llamamos la resolucion de la promesa
+            resolve();
+        }, 1000);
+
+        
+
+    });
+
+    //Nota: Por ejemplo simulando un caso en el que nuestro SW no se tiene que activar hasta que se termine la instalación, ya sea porque durante la instalación
+    //requerimos que se descarguen recursos necesarios los cuales si no son descargados durante la instalación nos podría arrojar un error la aplicación.
+    //Por lo tanto, esto lo podemos solucionar agregando la siguiente intrucción de código para esperar que finalice primero un evento, en este caso la instalación antes de realizar la activación del SW.
+    evento.waitUntil(instalacion);
 });
 
 //Evento de la activación del SW
