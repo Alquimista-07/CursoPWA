f64b64888e9ecdcb6251130196818d62ff64f397
Alquimista-07 <jdca.07@gmail.com>
2022-02-22 10:22:37 -0500

    Agregada explicación sobre el evento activate del SW y algunas notas importantes y ajustes en el evento install del SW

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -9,5 +9,24 @@ self.addEventListener('install', evento => {
     //Desargar assets
     //Creamos un cache
 
-    console.log('SW: Instaladno SW');
+    console.log('SW: Instalando SW');
+
+    //Si queremos que el nuevo SW tome el control inmediatamente que se instala, es decir, que el esperar a que
+    //el cliente cierre todos los tabs o la acción que hacemos en las herramientas de desarrollo de darle skip podemos
+    //hacer lo siguiente:
+    
+    //self.skipWaiting();
+    
+    //NOTA: No es recomendable dejarlo porque puede ser que el usuario este trabajando con algo del SW, por ejemplo recibir una notificación push,
+    //o algo importante que puede estar sucediendo que le interesa al usuario pero como saltamos lo del anterior SW pueda ser que no ejecute el codigo y
+    //perder la información importante que el cliente este esperando, lo mejor es que el cliente cierr la app web para que el nuevo SW entre en funcionamiento
+});
+
+//Evento de la activación del SW
+//Cuando el SW toma el control de la aplicación
+self.addEventListener('activate', evento => {
+    
+    // Usualmente el evento activate es un buen momento para:
+    //Borrar cache viejo
+    console.log('SW: Activo y listo para controlar la App');
 });
