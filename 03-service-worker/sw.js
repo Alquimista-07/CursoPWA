4bf575201aa3b48c758e6564cea342b1c440ca86
Alquimista-07 <jdca.07@gmail.com>
2022-02-22 09:41:19 -0500

    Explicación evento install de un SW

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -1,3 +1,13 @@
 
 // Ciclo de vida del SW
 
+// Evento de la instalación
+self.addEventListener('install', evento => {
+
+    //Normalmente en el evento de la instalación nosotros hacemos cosas como:
+
+    //Desargar assets
+    //Creamos un cache
+
+    console.log('SW: Instaladno SW');
+});
