c263af1b2131d41c9aa7ebc8f5cc7d5047e3a480
Alquimista-07 <jdca.07@gmail.com>
2022-02-23 15:04:24 -0500

    Incio del proyecto y respuesta offline básica

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,3 +1,16 @@
+//Detectamos cuando no tenemos una conexión o cuando la conexión falla
+self.addEventListener('fetch', event =>{
 
+    const offlineResp = new Response(`
+        Bienvenido a Mi Página Web.
 
+        Disculpa, pero para usarla, necesitas conexión a internet
+    `);
 
+    //Acá lo que se esta haciendo es que para cada petición fetch regrese exáctamente lo mismo que se esta pidiendo
+    const resp = fetch(event.request)
+        .catch( () => offlineResp);
+
+    event.respondWith(resp);
+
+});
