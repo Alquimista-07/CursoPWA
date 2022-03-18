aba079ae8efddef5176bd824c5c01dc7972fdd86
Alquimista-07 <jdca.07@gmail.com>
2022-03-18 10:30:30 -0500

    Manejo de errores e inserción de registros forma tediosa sin librerias de terceros

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -27,6 +27,70 @@ request.onupgradeneeded = event => {
         keyPath: 'id'
     });
 
-}
+};
+
+// Manejo de errores
+request.onerror = event => {
+
+    console.log('DB error: ', event.target.error);
+
+};
+
+// Insertar datos en la DB
+request.onsuccess = event => {
+
+    let db = event.target.result;
+
+    // Creamos un arreglo de la información que queremos postear a nuestra
+    // DB local
+    let heroesData = [
+        {
+            id: '1111',
+            heroe: 'Spiderman',
+            mensaje: "Aquí su amigo Spiderman"
+        },
+        {
+            id: '2222',
+            heroe: 'Iron Man',
+            mensaje: "Aquí en mi nuevo Mark 50"
+        }
+    ];
+
+    // Ahora para grabar los datos tengo que crear una transacción
+    // y la cual recibe 2 argunentos, el primero es la referencia al objeto donde vamos a guardar,
+    // en el segundo tengo que especificar si la transacción es de lectura o de lectura y escritura
+    //
+    // readonly = Solo lectura
+    // readwrite = Lectura y escritura
+    //
+    let heroesTransaction = db.transaction( 'heroes', 'readwrite');
+
+    // Esta transacción podría fallar por alguna razón
+    // por lo tanto manejamos el error
+    heroesTransaction.onerror = event => {
+        console.log('Error guardando los datos', event.target.error);
+    };
+
+    // Si la transacción se realiza de forma exitosa, informamos sobre el éxito de la trx
+    heroesTransaction.oncomplete = event => {
+        console.log('Transacción exitosa!!!...', event);
+    };
+
+    // Ahora necesitamos un objeto de esa transación
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    //Ahora vamos a barrer el arreglo para insertra los registros 1 por 1
+    // y con esto hacemos la insersión
+    for ( let heroe of heroesData ){
+        heroesStore.add( heroe );
+    }
+
+    // Ahora si la inserción se hace
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado a la base de datos');
+    };
+
+
+};
 
 }
