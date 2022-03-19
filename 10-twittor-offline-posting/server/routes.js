eadb7d80b4170049396ca6d418e8a7586957273a
Alquimista-07 <jdca.07@gmail.com>
2022-03-19 09:47:29 -0500

    Agregado API REST - Get Mensajes con su correspondiente explicación y ajustes

--- 10-twittor-offline-posting/server/routes.js
+++ 10-twittor-offline-posting/server/routes.js
@@ -2,15 +2,46 @@
 var express = require('express');
 var router = express.Router();
 
-
-
+// Nos creamos un pequeño arreglo de mensajes, estos mensajes bien podrían estar almacenados
+// en una base de datos cualquiera, pero en este ejercicio lo bamos a manejar como un arreglo
+// para que sea bastante sencillo.
+// NOTA: Cabe resaltar que estos mensajes no se estan almacenando de forma persistente y cada vez que
+//       recargue el navegador web, estos se van a purgar
+const mensajes = [
+  {
+    _id: '001',
+    user: 'Spiderman',
+    mensaje: 'Hola! les habla su amigo Spiderman.'
+  },
+  {
+    _id: '002',
+    user: 'Iron Man',
+    mensaje: 'Les habla Iron Man desde su nuevo Mark 50.'
+  },
+  {
+    _id: '003',
+    user: 'Hulk',
+    mensaje: 'Mi secreto es que siempre estoy furioso.'
+  },
+  {
+    _id: '004',
+    user: 'Thor',
+    mensaje: 'Un rey sabio nunca busca la guerra, pero debe estar preparado.'
+  }
+];
 
 
 
 
 // Get mensajes
+// Para probar esta ruta get mensajes con ayuda de postman pasamos la siguiente ruta http://localhost:3000/api
+// y la enviamos para ver el resultado.
+// NOTA: Estamos indicando /api en la ruta porque en el archivo server.js en las rutas le estamos indicando que se va a acceder
+//       a él a través de /api
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  //res.json('Obteniendo mensajes');
+  // Para obtenere los mensajes comentamos la antrerior respuesta y hacemos lo siguiente:
+  res.json( mensajes );
 });
 
 
