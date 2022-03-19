ceb4b532941968532aecf6c996b37c63c4856502
Alquimista-07 <jdca.07@gmail.com>
2022-03-19 08:49:22 -0500

    Solución ejercicio entrenamiento con PouchDB

--- 09-pouchdb-manual/app.js
+++ 09-pouchdb-manual/app.js
@@ -4,7 +4,7 @@
 
 // 1- Crear la base de datos
 // Nombre:  mensajes
-
+var db = new PouchDB('mensajes');
 
 
 // Objeto a grabar en base de datos
@@ -17,26 +17,59 @@ let mensaje = {
 
 
 // 2- Insertar en la base de datos
-
+db.put( mensaje )
+      .then( console.log('Registro Insertado!!!...') )
+              .catch( console.log )
 
 
 // 3- Leer todos los mensajes offline
-
-
+db.allDocs( { include_docs: true, descending: false} )
+    .then(doc => {
+        console.log("El registro es:", doc.rows);
+});
 
 
 // 4- Cambiar el valor 'sincronizado' de todos los objetos
 //  en la BD a TRUE
+db.allDocs( { include_docs: true, descending: false} )
+    .then(docs => {
+        
+        docs.rows.forEach(row => {
 
+            //console.log(row.doc);
+            let doc = row.doc;
+            
+            doc.sincronizado = true;
+            db.put(doc);
+            console.log('Registro Actualizado!!!...');
+            console.log(row.doc);
 
+        });
+
+});
 
 
 // 5- Borrar todos los registros, uno por uno, evaluando
 // cuales estan sincronizados
 // deberá de comentar todo el código que actualiza
-// el campo de la sincronización 
+// el campo de la sincronización
+db.allDocs( { include_docs: true } )
+    .then(docs => {
+        
+        docs.rows.forEach(row => {
 
+            let doc = row.doc;
+            
+            if ( doc.sincronizado ){
+                //console.log('Entro');
+                //console.log(doc.sincronizado);
+                db.remove(doc);
+                console.log('Mensaje Eliminado Exitosamente');
+            }else{
+                console.log('El Registro aún no se ha sincronizado, por tanto no fue borrado');
+                console.log(doc);
+            }
+           
+        });
 
-
-
-
+});
