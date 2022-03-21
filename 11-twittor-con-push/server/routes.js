26ccfaac1fd94c24e8c5b25db51701a87d66a289
Alquimista-07 <jdca.07@gmail.com>
2022-03-21 18:12:55 -0500

    Definir los servicios REST necesarios - PUSH - SUBSCRIBE - KEY

--- 11-twittor-con-push/server/routes.js
+++ 11-twittor-con-push/server/routes.js
@@ -40,6 +40,37 @@ router.post('/', function (req, res) {
   });
 });
 
+// Empezamos con las configuraciones que vamos a necesitar en nuestro backend para hacer toda la suscripci´n
+// y poder enviar notificaciones push.
+
+// Ahora necesito varias rutas nuevas
+
+// Almacenar la subscripción
+router.post('/subscribe', (req, res) => {
+
+  res.json('subscribe');
+
+});
+
+// Obtener el key publico
+router.get('/key', (req, res) => {
+
+  res.json('key publico');
+
+}); 
+
+// Enviar una notificación PUSH a las personas que nosotros
+// queramos.
+// NOTA:  Normalmente es no es hecho mediante un servicio REST 
+//        ya que esto ES ALGO que se controla del lado del server.
+//NOTA 2: Por lo tanto esta ruta /push me va a ayudar a mi a poder enviar 
+//        notificaciones PUSH mediante Postman ya que normalmete este no es un
+//        servicio que este expuesto ya que sería algo que solo corra nuestro backend
+router.post('/push', (req, res) => {
+
+  res.json('push');
+
+}); 
 
 
 module.exports = router;
