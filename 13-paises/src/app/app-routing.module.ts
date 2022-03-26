d6554872bd956569bb138cfc5d06ca4198e3568b
Alquimista-07 <jdca.07@gmail.com>
2022-03-26 11:49:26 -0500

    145. Rutas de nuestra aplicación

---
+++ 13-paises/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PaisesComponent } from './pages/paises/paises.component';
+import { PaisComponent } from './pages/pais/pais.component';
+
+// Configuramos la rutas
+const routes: Routes = [
+  {
+    path: '',
+    component: PaisesComponent
+  },
+  {
+    path: 'pais/:id',
+    component: PaisComponent
+  },
+  {
+    path: '**',
+    component: PaisesComponent
+  }
+];
+
+@NgModule({
+  // Importamos los modulos a las rutas principales de mi aplicación
+  imports: [ RouterModule.forRoot( routes ) ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
